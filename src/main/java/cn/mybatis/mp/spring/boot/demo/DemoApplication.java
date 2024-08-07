103f6c477995618e2723fa44d53749c63c0d76c1
Ai东 <abc-127@live.cn>
2024-08-07 17:34:21 +0800

    1.6.2-rc2

--- src/main/java/cn/mybatis/mp/spring/boot/demo/DemoApplication.java
+++ src/main/java/cn/mybatis/mp/spring/boot/demo/DemoApplication.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @Configurable
 @MapperScan("cn.mybatis.mp.spring.boot.demo.mapper")
 @SpringBootApplication
