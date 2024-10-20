5de68ee0a4d503ac32cbf0cc85a6fcb6a0353de4
Ai东 <abc-127@live.cn>
2024-10-20 11:54:41 +0800

    优化

--- src/main/java/cn/mybatis/mp/spring/boot/demo/DemoApplication.java
+++ src/main/java/cn/mybatis/mp/spring/boot/demo/DemoApplication.java
@@ -1,7 +1,6 @@
 package cn.mybatis.mp.spring.boot.demo;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
